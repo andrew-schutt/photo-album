161a6a881c2236a172b7221c8151943bac580c6b
Andrew Schutt <schutt.andrew@protonmail.com>
2023-10-27 15:35:45 -0500

    updates to project
    - adds a .ruby-version file
    - updates the script to handle filter params
    - additions to README to document filtering

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Lean Techniques showcase
 
 Hello!
 
-This is a Ruby script accomplishes the task for Option 1 for the LT Technical Showcases
+This is Ruby script accomplishes the task for the LT Technical Showcases
 
 ## Setup
 
@@ -13,6 +13,8 @@ To run this solution ensure that you have Ruby installed on your machine
 > ruby -v
 
 If this doesn't return output similar to this `ruby 2.6.5p114 (2019-10-01 revision 67812) [x86_64-darwin19]` then you might not have Ruby installed on your system.
+
+**NOTE** The default installed version of ruby on MacOS should be good. If you run into issues with ruby versioning you may need to install an older version.
 
 Here is the offical Ruby-Lang download/install instructions https://www.ruby-lang.org/en/downloads/
 
@@ -32,8 +34,29 @@ Once you have Ruby setup and installed you can run the script like so from your 
 
 Where `<ALBUM ID ARGUMENT>` is an integer for an album id you wish to return
 
+### Additional Filtering
+
+Additionally you can add additional filtering of the photos within an album by any of the attributes that make up a photo.
+
+To add the extra filtering you can pass in the attribute name and value you're looking for
+
+```bash
+./photo-album.rb <ALBUM ID ARGUMENT> <FILTER BY ARGUEMENT> <VALUE SEARCHING FOR>
+```
+
+To search the first album for the thumbnailUrl of https://via.placeholder.com/150/9e59da you would pass in
+
+```bash
+./photo-album.rb 1 thumbnailUrl https://via.placeholder.com/150/9e59da
+```
+
+Here would be the output from that filter
+
+`[50] et inventore quae ut tempore eius voluptatum`
+
 ### NOTES
 
 - Running the script to retrieve photos from a specific album will also run the tests.
 - Giving no album id input will return all 5000 photos
 - Giving an invalid album id with zero results returns a simple error message
+- Providing no filter args will output a simple informative message
--- photo-album.rb
+++ photo-album.rb
@@ -32,7 +32,7 @@ class PhotoAlbumTest < Minitest::Test
   def test_parse_photo_album_method
     photo_album = PhotoAlbum.new('url')
     raw_json = "[\n {\n \"albumId\": 1,\n \"id\": 1,\n \"title\": \"awesome title\",\n \"url\": \"https://via.placeholder.com/600/92c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/92c952\"\n}]"
-    assert photo_album.parse_photo_album(raw_json) == [{:id=>1, :title=>"awesome title"}]
+    assert photo_album.parse_photo_album(raw_json, photo_album.build_filter_lambda(nil, nil)) == [{:id=>1, :title=>"awesome title"}]
   end
 
   def test_print_photos_method
@@ -45,6 +45,35 @@ class PhotoAlbumTest < Minitest::Test
     photo_album = PhotoAlbum.new('url')
     parsed_album = []
     assert_output("Hmmm... no albums with that id :(\n") { photo_album.print_photos(parsed_album) }
+  end
+
+  def test_filter_lambda_filters_with_params
+    photo_album = PhotoAlbum.new('url')
+    filter_lambda = photo_album.build_filter_lambda('id', 1)
+    raw_json = "[\n {\n \"albumId\": 1,\n \"id\": 1,\n \"title\": \"awesome title\",\n \"url\": \"https://via.placeholder.com/600/92c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/92c952\"\n}, {\n \"albumId\": 1,\n \"id\": 2,\n \"title\": \"super title\",\n \"url\": \"https://via.placeholder.com/600/94c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/94c952\"\n}]"
+    json_hash = JSON.parse(raw_json)
+    assert json_hash.filter { |val| filter_lambda.call(val) }.size == 1
+  end
+
+  def test_filter_lambda_does_not_filter_without_params
+    photo_album = PhotoAlbum.new('url')
+    filter_lambda = photo_album.build_filter_lambda(nil, nil)
+    raw_json = "[\n {\n \"albumId\": 1,\n \"id\": 1,\n \"title\": \"awesome title\",\n \"url\": \"https://via.placeholder.com/600/92c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/92c952\"\n}, {\n \"albumId\": 1,\n \"id\": 2,\n \"title\": \"super title\",\n \"url\": \"https://via.placeholder.com/600/94c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/94c952\"\n}]"
+    json_hash = JSON.parse(raw_json)
+    assert json_hash.filter { |val| filter_lambda.call(val) } == json_hash
+  end
+
+  def test_filter_lambda_with_invalid_params
+    photo_album = PhotoAlbum.new('url')
+    filter_lambda = photo_album.build_filter_lambda('doesnt', 'exist')
+    raw_json = "[\n {\n \"albumId\": 1,\n \"id\": 1,\n \"title\": \"awesome title\",\n \"url\": \"https://via.placeholder.com/600/92c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/92c952\"\n}, {\n \"albumId\": 1,\n \"id\": 2,\n \"title\": \"super title\",\n \"url\": \"https://via.placeholder.com/600/94c952\",\n \"thumbnailUrl\": \"https://via.placeholder.com/150/94c952\"\n}]"
+    json_hash = JSON.parse(raw_json)
+    assert json_hash.filter { |val| filter_lambda.call(val) }.size == 0
+  end
+
+  def test_print_message_when_no_filters_params_given
+    photo_album = PhotoAlbum.new('url')
+    assert_output("No additional filtering provided (you can filter an album by any fields on the photo)\n") { photo_album.build_filter_lambda(nil, nil) }
   end
 end
 
@@ -60,9 +89,10 @@ class PhotoAlbum
     @album_param = query_param_key
   end
 
-  def retrieve(album_id)
+  def retrieve(album_id, filter_by, filter_val)
     response = get_response(build_url(album_id))
-    print_photos(parse_photo_album(response))
+    filter_lambda = build_filter_lambda(filter_by, filter_val)
+    print_photos(parse_photo_album(response, filter_lambda))
   end
 
   def build_url(album_id=nil)
@@ -75,9 +105,9 @@ class PhotoAlbum
     Net::HTTP.get(uri)
   end
 
-  def parse_photo_album(raw_response)
+  def parse_photo_album(raw_response, filter_lambda)
     json_hash = JSON.parse(raw_response)
-    json_hash.map{ |photo| {id: photo['id'], title: photo['title']}}
+    json_hash.filter { |photo| filter_lambda.call(photo) }.map{ |photo| {id: photo['id'], title: photo['title']} }
   end
 
   def print_photos(photos)
@@ -87,8 +117,17 @@ class PhotoAlbum
       photos.each{ |photo| puts "[#{photo[:id]}] #{photo[:title]}" }
     end
   end
+
+  def build_filter_lambda(filter_by, filter_val)
+    if filter_by.nil? || filter_val.nil?
+      puts "No additional filtering provided (you can filter an album by any fields on the photo)"
+      ->(photo) { photo }
+    else
+      ->(photo) { photo[filter_by].to_s == filter_val.to_s }
+    end
+  end
 end
 
 photo_album = PhotoAlbum.new('https://jsonplaceholder.typicode.com/photos', 'albumId')
 
-photo_album.retrieve(ARGV[0])
+photo_album.retrieve(ARGV[0], ARGV[1], ARGV[2])
